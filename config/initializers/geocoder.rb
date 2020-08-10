9ec7d3d7cb999eb974b2139517bea1665343f59c
Takumi Endo <endotaku0608@gmail.com>
2020-08-10 00:09:25 +0000

    [Add]env

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -7,7 +7,7 @@ Geocoder.configure(
   # ip_lookup: :maxmind,
 
   # to use an API key:
-  api_key: "AIzaSyDHE7J0_xmhl6aqKXBSGNJas1sY9QV0rzA",
+  api_key:ENV['API_KEY'],
 
   # geocoding service request timeout, in seconds (default 3):
   timeout: 5,
